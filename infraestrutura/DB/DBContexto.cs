dd9dfbaefa18a32b9e92d97504096fb8400911a7
MrBarszcz <marcosbarszcz@gmail.com>
2024-11-04 17:39:08 -0300

    Iniciando Projeto

---
+++ infraestrutura/DB/DBContexto.cs
@@ -0,0 +1,10 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace minimal_api.infraestrutura.DB {
+    public class DBContexto {
+        
+    }
+}
